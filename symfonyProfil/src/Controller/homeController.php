88209eee7f19cb40c8ed20987a9e6c8b48949f02
youkhai <khaiway_la@hotmail.fr>
2020-05-29 12:41:11 +0200

    first commit

---
+++ symfonyProfil/src/Controller/homeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class homeController extends AbstractController  {
+
+    /**
+     * @Route("/", name="homepage")
+     */
+
+    public function home(){
+        return $this->render(
+            'home.html.twig'
+        );
+    }
+}
